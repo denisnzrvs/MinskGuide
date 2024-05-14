ac98856d61e93516d0530cf4d789be1b35ac7279
Denis Nazarovs <denis.nazarovs@va.lv>
2024-05-14 14:43:30 +0300

    Empty project structure

---
+++ MinskGuide/MinskGuide/MinskGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinskGuideApp.swift
+//  MinskGuide
+//
+//  Created by Denis Nazarovs on 13/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct MinskGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
